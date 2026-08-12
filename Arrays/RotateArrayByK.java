352ac7fd7f640b3e905b01bd86712f1980466206
Suresh_Ganapathy <sureshganapathy5349@gmail.com>
2026-08-12 20:48:29 +0530

    Rotate Array by K rotation problem completed

---
+++ Arrays/RotateArrayByK.java
@@ -0,0 +1,64 @@
+/*
+Problem:
+Left rotate an array by K position
+
+Approach:
+We will take the value at 0th index and keep it in an temp variable and will make all the elements to shift towards 1 position left and at last we add the temp value at the last position.
+
+Time Complexity:
+O(N)
+
+Space Complexity:
+O(1)
+*/
+
+package Arrays;
+
+public class RotateArrayByK {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 4, 5};
+		int k = 8;
+		
+		int size = arr.length;
+		
+		if (k > size){
+			k = k % size;
+		}
+		
+		// System.out.println(k);
+		
+		int answer [] = new int [size] ;
+		
+		answer = reverse(arr,0,size-1);
+		answer = reverse(answer,0, k-1);
+		answer = reverse(answer,k , size -1);
+		
+		System.out.println();
+		System.out.print("Array after rotate by K position :");
+		
+		for(int i = 0; i < size ; i++){
+			System.out.print(" " + answer[i]);
+		}
+		
+		System.out.println();
+	}
+  
+	
+	public static int[] reverse(int arr[], int start, int end){
+		
+		while(start < end ){
+		
+		int temp = arr[start];
+		arr[start] = arr[end];
+		arr[end] = temp;
+		
+		start++;
+		end--;
+		
+		}
+	
+		return arr;
+	}	
+}
